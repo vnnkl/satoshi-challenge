5c58aa4591d7d6b5b3ac2410cc88e3e65613c64f
Constantin Vennekel <constantin.vennekel@bitwala.com>
2024-05-23 12:54:24 +0200

    Refactor Button component for improved styling
    Import React, update props interface, and enhance button styles.

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -1,3 +1,9 @@
-export const Button = () => {
-  return <button className="text-lg bg-red-500">TestButton</button>;
+import React from 'react';
+
+interface ButtonProps {
+  children: any;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children }) => {
+  return <button className="pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">{children}</button>;
 };
